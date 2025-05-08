f6f9f589243b102e533ddd403d91cf834684a4f9
igor.lazzarettii <idlazzaretti@gmail.com>
2025-05-07 22:53:40 -0300

    :scroll: Hermione: Dicas em shell script Atualização do Readme, atualização da opção 7 e da 1 com dicas linux.

--- hermione.sh
+++ hermione.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
-# Bem-vindo à Hermione, a assistente mágica de dicas!
+# Bem-vindo à Hermione, sua assistente no Linux!
 while true; do
     echo "Olá, sou Hermione! Lembre-se, estas dicas não substituem sua ida para a biblioteca."
     echo "Escolha uma opção abaixo:"
-    echo "1 - Dica Linux"
-    echo "2 - Dica Git"
-    echo "3 - Dica Alias"
-    echo "4 - Dica Podman"
-    echo "5 - Dica Postgres"
-    echo "6 - Dica SQL"
+    echo "1 - Dicas Linux"
+    echo "2 - Dicas Git"
+    echo "3 - Dicas Alias"
+    echo "4 - Dicas Podman"
+    echo "5 - Dicas Postgres"
+    echo "6 - Dicas SQL"
+    echo "7 - Atualizar meu sistema"
     echo "0 - Sair"
 
     # Solicitar a entrada do usuário
-    read -p "Digite sua escolha (0-6): " opcao
+    read -p "Digite sua escolha (0-7): " opcao
 
     # Função para perguntar se o usuário quer continuar
     perguntar_continuar() {
@@ -37,16 +38,26 @@ while true; do
     # Processar a escolha do usuário
     case $opcao in
         1)
-            echo "Dica Linux: Use 'ls -lah' para listar arquivos com detalhes."
+            echo "Estas dicas serão uteis para navegar no mundo Linux."
+            echo ""
+            echo "[comando] --help --- Ajuda. Lista as opções do comando pretendido"
+            echo "ls -a            --- Lista todos os arquivos ocultos"
+            echo "ls -lh           --- Lista arquivos com seu tamanho em formato legível"
+            echo "rm -rf           --- Remove pasta recursivamente e forçadamente"
+            echo "cp               --- Copia um arquivo para o local desejado"
+            echo "cat [nome].txt   --- Concatena criando um novo arquivo"
+
             echo "Ainda precisa de ajuda? Talvez algumas horas na biblioteca resolvam..."
             perguntar_continuar
             ;;
         2)
+        # echo "2 - Dica Git"
             echo "Dica Git: Use 'git log --oneline --graph' para visualizar o histórico de commits."
             echo "Ainda precisa de ajuda? Talvez algumas horas na biblioteca resolvam..."
             perguntar_continuar
             ;;
         3)
+        # echo "3 - Dica Alias"
             echo "Dica Alias: Use 'alias ll=\"ls -lah\"' para criar um atalho para listar arquivos."
             echo "Ainda precisa de ajuda? Talvez algumas horas na biblioteca resolvam..."
             perguntar_continuar
@@ -63,6 +74,16 @@ while true; do
             ;;
         6)
             echo "Dica SQL: Use 'SELECT * FROM tabela LIMIT 10;' para visualizar os primeiros 10 registros."
+            echo "Ainda precisa de ajuda? Talvez algumas horas na biblioteca resolvam..."
+            perguntar_continuar
+            ;;
+        7)
+            echo "Vamos atualizar o seu Sistema Operacional Fedora."
+            echo ""
+            echo "Executando o comando 'sudo dnf upgrade --refresh'..."
+            sudo dnf upgrade --refresh -y
+
+            echo "Atualização finalizada com sucesso!"
             echo "Ainda precisa de ajuda? Talvez algumas horas na biblioteca resolvam..."
             perguntar_continuar
             ;;
